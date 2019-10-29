208d97136e3ed65ca76f7a7dfaef59e570519a0d
MBuchalik <dev@martin-buchalik.de>
2019-10-29 17:16:03 +0100

    Add AllTests.java and test suites for all test packages

---
+++ src/test/java/org/wahlzeit/AllTests.java
@@ -0,0 +1,24 @@
+package org.wahlzeit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.wahlzeit.handlers.HandlersTestSuite;
+import org.wahlzeit.model.ModelTestSuite;
+import org.wahlzeit.model.persistence.ModelPersistenceTestSuite;
+import org.wahlzeit.services.ServicesTestSuite;
+import org.wahlzeit.services.mailing.ServicesMailingTestSuite;
+import org.wahlzeit.utils.UtilsTestSuite;
+
+@RunWith(Suite.class)
+
+@SuiteClasses({
+  HandlersTestSuite.class,
+  ModelTestSuite.class,
+  ModelPersistenceTestSuite.class,
+  ServicesTestSuite.class,
+  ServicesMailingTestSuite.class,
+  UtilsTestSuite.class
+})
+
+public class AllTests {}
---
+++ src/test/java/org/wahlzeit/model/persistence/ModelPersistenceTestSuite.java
@@ -0,0 +1,13 @@
+package org.wahlzeit.model.persistence;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+
+@SuiteClasses({
+  DatastoreAdapterTest.class
+})
+
+public class ModelPersistenceTestSuite {}
